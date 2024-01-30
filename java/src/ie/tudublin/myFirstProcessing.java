20bfa1d07356c485ba3be53c4184cb9fccc73153
AniketTUD <C22448826@mytudublin.ie>
2024-01-30 14:50:33 +0000

    implemented myFirstProcessing

--- java/src/ie/tudublin/myFirstProcessing.java
+++ java/src/ie/tudublin/myFirstProcessing.java
@@ -11,11 +11,22 @@ public class myFirstProcessing extends PApplet
 	}
 
 	public void setup() {
-
+		background(255, 0, 0);
 	}
 	
 	public void draw()
-	{	
-		
+	{
+		fill(255, 255, 0);
+		stroke(255, 255, 0);	
+		circle(250, 325, 350);
+		fill(0, 255, 255);
+		stroke(0, 255, 255);
+		triangle(250, 100, 50, 450, 450, 450);
+		fill(250);
+		stroke(250);
+		ellipse(250, 250, 150, 75);
+		fill(0);
+		stroke(0);
+		circle(250, 250, 50);
 	}
 }
